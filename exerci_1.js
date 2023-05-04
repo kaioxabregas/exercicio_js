882cb3658d928ad71d5892228b8a693842383212
kaioxabregas <kaio_lobato@hotmail.com>
2023-05-04 12:06:37 -0300

    inicio

---
+++ exerci_1.js
@@ -0,0 +1,16 @@
+console.log('Resultado do Exercicio 1');
+const n1 = 2148
+let n2;
+
+console.log(`O valor da 1ª variavel é ${n1}!`)
+
+n2 = 25
+
+console.log(`Minha 2ª variavel vale ${n2} e o valor da soma das 2 variaveis é ${n1+n2}`)
+
+n2 = n2**2
+console.log(n2)
+
+n2 = n2/3
+console.log(n2)
+
